fe0abf033cb5c143f34c63863d14da57b2f2b863
Vasili <vassilcom@gmail.com>
2019-08-28 02:08:46 +0200

    1. Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+//for use by Hazel applications
+
+#include "Hazel/Application.h"
+
+//------Entry Point----
+#include "Hazel/EntryPoint.h"
+//------------------
---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+// something that we will define somewhere in client.
+
+int main(int argc, char** argv)
+{
+	
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
